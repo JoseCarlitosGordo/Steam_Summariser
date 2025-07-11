ce8e4fae972f69a0ee7696bdade3363a40bb77f3
JoseCarlitosGordo <165242859+JoseCarlitosGordo@users.noreply.github.com>
2025-07-11 14:12:59 +0930

    figured out how steam's search_games function works

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,13 @@
-from flask import flas
+from flask import Flask 
+from steam_web_api import Steam
+
+KEY = "532137961076F4F8A8E5840E5C552DCA"
+
+steam = Steam(KEY)
+
+
+print(steam.users.search_user("DahoovieDoober"))
+games = steam.apps.search_games("terr")
+for game in games["apps"]:
+    print(f"{game} \n")
+
