6cb25bf0cef524a81474c7254062c6c68624cf77
JoseCarlitosGordo <165242859+JoseCarlitosGordo@users.noreply.github.com>
2025-07-20 21:48:34 +0930

    created a render_template so that the backend returns output smoothly to a template html file

--- Backend/main.py
+++ Backend/main.py
@@ -1,13 +1,31 @@
-from flask import Flask 
+from flask import Flask, jsonify, render_template
+from requests import get
 from steam_web_api import Steam
-
+app = Flask(__name__, template_folder="/templates")
 KEY = "532137961076F4F8A8E5840E5C552DCA"
 
 steam = Steam(KEY)
 
+class Searcher:
+    @app.route("/")
+    @staticmethod
+    def index():
+        return render_template("index.html")
 
-print(steam.users.search_user("DahoovieDoober"))
-games = steam.apps.search_games("terr")
-for game in games["apps"]:
-    print(f"{game} \n")
+    @app.route("/find_game", strict_slashes=False, methods=["POST"])
+    @staticmethod
+    def find_game(search_parameter):
+        game_list = []
+        games = steam.apps.search_games(search_parameter)
+        
+        for game in games["apps"]:
+            game_list.append(game)
+        return jsonify(game_list)
+
+
+    
+# print(steam.users.search_user("DahoovieDoober"))
+# games = steam.apps.search_games("terr")
+# for game in games["apps"]:
+#     print(f"{game} \n")
 
